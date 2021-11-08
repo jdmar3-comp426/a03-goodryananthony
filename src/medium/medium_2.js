1d7dcee125eb11777f394a1f3df9a2263cc7ea3d
goodryananthony <goodryan@live.unc.edu>
2021-11-08 17:15:12 -0500

    removed spicy reference

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -1,6 +1,5 @@
 import mpg_data from "./data/mpg_data.js";
 import {getStatistics} from "./medium_1.js";
-import {everyEven} from "../spicy/spicy_9";
 
 /*
 This section can be done by using the array prototype functions.
