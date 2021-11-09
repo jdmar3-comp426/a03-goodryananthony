9464e47b8ed080d39f0fb5c7484e60252c57b0fb
goodryan <goodryan@live.unc.edu>
2021-11-09 16:05:21 -0500

    doing some mild

--- src/test.js
+++ src/test.js
@@ -2,13 +2,16 @@
 // eslint-disable-next-line no-global-assign
 require = require("esm")(module/* , options */)
 module.exports = require("./main.js")
+import {sumToString, getIncreasingArray, maxAndMin} from "./mild/mild_1";
 import {getMedian, getStatistics} from "./medium/medium_1";
 import {allCarStats, makerHybrids, moreStats} from "./medium/medium_2";
 
 let people = [4, 2, 100, 200, 3, 1, 5];
 
 
-console.log(allCarStats);
+console.log(sumToString(3, 9));
+console.log(getIncreasingArray(3, 7));
+console.log(maxAndMin(people));
 
 
 
